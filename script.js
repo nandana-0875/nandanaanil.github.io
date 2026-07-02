cc3dfd6b9bdcf2d8911fb6cd361ee84737e4da9a
nandana-0875 <nandana87500@gmail.com>
2026-07-02 22:46:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Dynamic scroll handler for the navigation header
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        document.body.classList.add('scrolled');
+    } else {
+        document.body.classList.remove('scrolled');
+    }
+});
